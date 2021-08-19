414a9aaf186a1e125d9ea82fd532ae5ff22cdca0
karolykertesz <kforcegroup@hotmail.com>
2021-08-19 02:40:40 +0200

    first commit

---
+++ front/src/utils/firebase.ts
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: "kertesz-portfolio",
+  storageBucket: process.env.REACT_APP_FIREBASE_SORAGE_BUCKET,
+  messagingSenderId: "264755473485",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: "G-KSB4YQHGWK",
+};
+
+export const appInit = (): void => {
+  if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
+};
